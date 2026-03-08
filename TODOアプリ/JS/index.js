49ecba6660ac30a0320324fd495614ef13be09b5
奥村聖孔 <>
2026-03-08 16:31:38 +0900

    入力されたtodoの内容を画面に表示（この段階では同じものが複数表示されてしまうバグが発生する）

--- TODOアプリ/JS/index.js
+++ TODOアプリ/JS/index.js
@@ -18,6 +18,25 @@ document.addEventListener("DOMContentLoaded", () => {
 
         new_todoList.push(todoObject);
         console.log(new_todoList);
+
+        new_todoList.forEach((todo) => {
+            const todoListElement = document.createElement("td");
+            todoListElement.textContent = todo.listItem;
+            document.getElementById("todo-list").appendChild(todoListElement);
+
+            const titleElement = document.createElement("td");
+            titleElement.textContent = todo.title;
+            document.getElementById("todo-list").appendChild(titleElement);
+
+            const limitElement = document.createElement("td");
+            limitElement.textContent = todo.limit;
+            document.getElementById("todo-list").appendChild(limitElement);
+
+            const trElement = document.createElement("tr");
+            trElement.appendChild(todoListElement);
+            trElement.appendChild(titleElement);
+            trElement.appendChild(limitElement);
+            document.getElementById("todo-list").appendChild(trElement);
         });
 });
-
+});
